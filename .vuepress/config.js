b137fae3b82d1c7cec34d4428a25ada5bf7a0682
graysonchiang <abken7642@gmail.com>
2019-12-29 00:54:25 +0800

    add ch6

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,7 +6,9 @@ module.exports = {
     ],
     base: '/note/',
     description: '臺北大學 商學院企業管理碩士班 107',
-    plugins: ['vuepress-plugin-export'],
+    plugins: [
+        'flowchart'
+        ],
     markdown: {
         config: md => {
             md.use(require('markdown-it-katex'));
