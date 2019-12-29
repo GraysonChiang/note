50ccdc4f56ec16907d4e9aaa7ce680200273e3bb
graysonchiang <abken7642@gmail.com>
2019-12-29 13:18:06 +0800

    remvoe nessary package

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,17 +1,11 @@
 module.exports = {
     title: '企業管理碩士班筆記',
     head: [
-        // ['link', {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css'}],
         ['link', {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css'}],
     ],
     base: '/note/',
     description: '臺北大學 商學院企業管理碩士班 107',
-    plugins: ['mathjax'],
-    // markdown: {
-        // config: md => {
-            // md.use(require('markdown-it-katex'));
-        // },
-    // },
+    plugins: ['mathjax','flowchart'],
     themeConfig: {
         nav: [
             {text: 'Github', link: 'https://github.com/GraysonChiang/note'}
